f32145414e95382aaae25a52d0f07266fd32d3ba
Alexander George Travis <wewerelikesharks@gmail.com>
2020-05-02 12:06:58 -0700

    Revert "switched route path to run through route/index.js - still delivering …"

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const path = require(`path`);
 const router = require(`express`).Router();
@@ -8,7 +8,7 @@ const apiRoutes = require(`./api`);
 router.use(`/api`, apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(`/`, (req, res) => {
+router.use((req, res) => {
   res.sendFile(path.join(__dirname, `../client/build/index.html`));
 });
 
