6713aff0d0b568c6b798630a3edde98013a8a251
Remy Guts <remyguts@gmail.com>
2020-05-05 16:03:23 -0700

    add hit and hightlight to logic

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,39 +1,46 @@
 import React from "react";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 import { InstantSearch } from "react-instantsearch-dom";
 import { SearchBox } from "react-instantsearch-dom";
 import { Hits } from "react-instantsearch-dom";
+import { Highlight } from "react-instantsearch-dom";
 
-const hit = ({ hit }) => (
+/*eslint-disable*/
+
+const Hit = ({ hit }) => (
   <div className="hit">
     <div className="hitImage">
       <img src={hit.image} />
     </div>
+    <div className="hitName">
+      <Highlight attribute="name" hit={hit} />
+    </div>
   </div>
 );
+
 const Content = () => (
   <div className="content">
-    <Hits hitComponent={hit} />
+    <Hits hitComponent={Hit} />
   </div>
 );
-function Search() {
-  return (
-    <InstantSearch
-      appId="UDOLFJ7GFM"
-      apiKey="b2ce42b70fbdb2904dcf85f8f2431640"
-      indexName="prod_BIY"
-    >
-      <header>
-        <SearchBox translations={{ placeholder: "Search Box" }} />
-      </header>
 
-      <main>
-        <Content />
-      </main>
-    </InstantSearch>
-  );
+class App extends Component {
+  render() {
+    return (
+      <InstantSearch
+        appId="UDOLFJ7GFM"
+        apiKey="b2ce42b70fbdb2904dcf85f8f2431640"
+        indexName="prod_BIY"
+      >
+        <header>
+          <SearchBox translations={{ placeholder: "Search Box" }} />
+        </header>
+
+        <main>
+          <Content />
+        </main>
+      </InstantSearch>
+    );
+  }
 }
 
-export default Search;
+export default App;
