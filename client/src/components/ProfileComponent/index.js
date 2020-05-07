13e58b807a568e96cff7c153c0052a52d6632113
Issouf Kiema <walybi03@yahoo.com>
2020-05-06 17:29:35 -0700

    styled the profile page

--- client/src/components/ProfileComponent/index.js
+++ client/src/components/ProfileComponent/index.js
@@ -6,28 +6,57 @@ import image from "../../assets/images/userTest.png";
 function ProfileComponent(props) {
   return (
     <div>
-      <p>Working</p>
-      <p className="username">{props.username}</p>
-      <Image src={image} alt="profile pic" />
-      <p>{props.location}</p>
-      <p>{props.email}</p>
-      <p>{props.telephone}</p>
-      <p>{props.status}</p>
-      {props.role !== undefined ? (
-        <ul>
-          {props.role.map(role => (
-            <li>{role}</li>
-          ))}
-        </ul>
-      ) : null}
+      <h3 className="title">Personal Info</h3>
+      <hr></hr>
+      <Image className="pic" src={image} alt="profile pic" />
+      <br></br>
+      <p className="username">Welcome, {props.username}</p>
 
-      {/* {props.posts !== undefined ? (
+      <div className="shade w-75">
+        <div className="info">
+          <span>Location:</span>
+          <p>{props.location}</p>
+        </div>
+
+        <div className="info">
+          <span>Email:</span>
+          <p>{props.email}</p>
+        </div>
+
+        <div className="info">
+          <span>Phone:</span>
+          <p>{props.telephone}</p>
+        </div>
+
+        <div className="info">
+          <span>Status:</span>
+          <p>{props.status}</p>
+        </div>
+
+        <div className="info">
+          <span>Role:</span>
+          {props.role !== undefined ? (
+            <ul>
+              {props.role.map(role => (
+                <li>{role}</li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </div>
+
+      <hr></hr>
+      <h3 className="title">Posts</h3>
+      <div className="shade w-75">
+        <p>This is a sample post</p>
+        {/* {props.posts !== undefined ? (
         <ul>
           {props.posts.map(post => (
             <li>{post.description}</li>
           ))}
         </ul>
       ) : null} */}
+      </div>
     </div>
   );
 }
