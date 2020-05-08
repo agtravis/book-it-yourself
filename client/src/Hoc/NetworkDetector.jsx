5beca8a33c6c4f5d35d2944e61fa96993cb7d892
George Travis <wewerelikesharks@gmail.com>
2020-05-08 11:53:39 -0700

    added online/offline detector - so much fun!

---
+++ client/src/Hoc/NetworkDetector.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import localForage from "localforage";
+import API from "../utils/API";
+
+export default function (ComposedComponent) {
+  class NetworkDetector extends Component {
+    state = {
+      isDisconnected: false,
+    };
+
+    componentDidMount() {
+      this.handleConnectionChange();
+      window.addEventListener("online", this.handleConnectionChange);
+      window.addEventListener("offline", this.handleConnectionChange);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener("online", this.handleConnectionChange);
+      window.removeEventListener("offline", this.handleConnectionChange);
+    }
+
+    handleConnectionChange = () => {
+      const condition = navigator.onLine ? "online" : "offline";
+      if (condition === "online") {
+        const webPing = setInterval(() => {
+          fetch("//google.com", {
+            mode: "no-cors",
+          })
+            .then(() => {
+              this.setState({ isDisconnected: false }, () => {
+                this.backOnline();
+                return clearInterval(webPing);
+              });
+            })
+            .catch(() => this.setState({ isDisconnected: true }));
+        }, 2000);
+        return;
+      }
+
+      return this.setState({ isDisconnected: true });
+    };
+
+    backOnline = () => {
+      localForage
+        .getItem(`postKey`)
+        .then(value => {
+          console.log(`postKey value`);
+          console.log(value);
+          if (value) {
+            localForage
+              .iterate((value, key, iterationNumber) => {
+                if (key === `postKey`) {
+                  for (const post of value) {
+                    API.addPost(post.post).then(postDb => {
+                      API.updateUserNewPost(post.user, { id: postDb.data._id })
+                        .then(userDb => {
+                          console.log(userDb);
+                        })
+                        .catch(err => console.error(err));
+                    });
+                  }
+                }
+              })
+              .then(() => {
+                console.log(`iteration complete`);
+                localForage
+                  .removeItem(`postKey`)
+                  .then(() => {
+                    console.log(`offline storage emptied`);
+                    this.setState({ transmitting: false });
+                  })
+                  .catch(err => console.error(err));
+              })
+              .catch(err => console.error(err));
+          }
+        })
+        .catch(err => console.error(err));
+    };
+
+    render() {
+      const { isDisconnected } = this.state;
+      return (
+        <div>
+          {isDisconnected && (
+            <div className="internet-error">
+              <p>Internet connection lost</p>
+            </div>
+          )}
+          <ComposedComponent {...this.props} />
+        </div>
+      );
+    }
+  }
+
+  return NetworkDetector;
+}
