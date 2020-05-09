ea1cc9092e6affd6c30beebc62a7de27a7818893
George Travis <wewerelikesharks@gmail.com>
2020-05-08 19:27:17 -0700

    added giphy to feed

--- client/src/components/FeedCard/index.js
+++ client/src/components/FeedCard/index.js
@@ -3,16 +3,30 @@ import Card from "react-bootstrap/Card";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import API from "../../utils/API";
 import { Redirect } from "react-router-dom";
+import axios from "axios";
 import "./style.css";
-
 
 class FeedCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       redirect: null,
-      id: ``
+      id: ``,
+      imgSrc: ``,
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?api_key=krsGO7xfBLF7bCmtRs7tpOLC0UjdzxcH&q=${this.props.location}`
+      )
+      .then(data => {
+        const num = Math.floor(Math.random() * 20);
+        console.log(data.data.data[0].images.fixed_width.url);
+        this.setState({ imgSrc: data.data.data[num].images.fixed_width.url });
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
@@ -24,8 +38,8 @@ class FeedCard extends Component {
           to={{
             pathname: redir,
             state: {
-              userID: this.state.id
-            }
+              userID: this.state.id,
+            },
           }}
         />
       );
@@ -35,66 +49,71 @@ class FeedCard extends Component {
     const endDate = new Date(this.props.endDate);
 
     return (
-        <Card className= "w-75 mb-3 mx-auto">
+      <Card className="w-75 mb-3 mx-auto">
         <div class="row no-gutters">
-        <div class="col-md-4">
-        <img src="..." class="card-img" alt="..." />
-        </div>
-        <div class="col-md-8">
-          <Card.Body>
-            <h1>{this.props.title}</h1>
-            {this.props.name ? (
-              <button className="btn btn-secondary btn-sm rounded"
-                onClick={() => {
-                  this.setState({
-                    redirect: `/userdetails`,
-                    id: this.props.author
-                  });
-                }}
-              >
-                {`Contact ${this.props.name}`}
-              </button>
-            ) : null}
-            {this.props.delete ? (
-              <button className="btn btn-secondary btn-sm rounded mb-1"
-                onClick={() =>
-                  this.props.deletePost(this.props.id, this.props.author)
-                }
-              >
-                DELETE
-              </button>
-            ) : null}
-            {this.props.delete && this.props.complete !== `negative` ? (
-              <div>
-                <button className="btn btn-secondary btn-sm rounded"
+          <div class="col-md-4">
+            <img src={this.state.imgSrc} class="card-img" alt="..." />
+          </div>
+          <div class="col-md-8">
+            <Card.Body>
+              <h1>{this.props.title}</h1>
+              {this.props.name ? (
+                <button
+                  className="btn btn-secondary btn-sm rounded"
+                  onClick={() => {
+                    this.setState({
+                      redirect: `/userdetails`,
+                      id: this.props.author,
+                    });
+                  }}
+                >
+                  {`Contact ${this.props.name}`}
+                </button>
+              ) : null}
+              {this.props.delete ? (
+                <button
+                  className="btn btn-secondary btn-sm rounded mb-1"
                   onClick={() =>
-                    this.props.togglePostStatus(
-                      this.props.id,
-                      this.props.complete
-                    )
+                    this.props.deletePost(this.props.id, this.props.author)
                   }
                 >
-                  TOGGLE STATUS
+                  DELETE
                 </button>
-                <p>Status: {this.props.complete}</p>
-              </div>
-            ) : null}
-            <p>Location: {this.props.location}.</p>
-            <p>
-              When:{" "}
-              {`${startDate.getMonth() +
-                1}/${startDate.getDate()}/${startDate.getFullYear()}`}
-              {this.props.endDate !== ``
-                ? ` until ${`${endDate.getMonth() +
-                    1}/${endDate.getDate()}/${endDate.getFullYear()}`}`
-                : null}
-              .
-            </p>
-            <p>Description: {this.props.description}</p>
-          </Card.Body>
+              ) : null}
+              {this.props.delete && this.props.complete !== `negative` ? (
+                <div>
+                  <button
+                    className="btn btn-secondary btn-sm rounded"
+                    onClick={() =>
+                      this.props.togglePostStatus(
+                        this.props.id,
+                        this.props.complete
+                      )
+                    }
+                  >
+                    TOGGLE STATUS
+                  </button>
+                  <p>Status: {this.props.complete}</p>
+                </div>
+              ) : null}
+              <p>Location: {this.props.location}.</p>
+              <p>
+                When:{" "}
+                {`${
+                  startDate.getMonth() + 1
+                }/${startDate.getDate()}/${startDate.getFullYear()}`}
+                {this.props.endDate !== ``
+                  ? ` until ${`${
+                      endDate.getMonth() + 1
+                    }/${endDate.getDate()}/${endDate.getFullYear()}`}`
+                  : null}
+                .
+              </p>
+              <p>Description: {this.props.description}</p>
+            </Card.Body>
           </div>
-          </div>
-        </Card>
+        </div>
+      </Card>
     );
   }
 }
