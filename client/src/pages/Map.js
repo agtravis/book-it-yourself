0aa0e5d042a0430b66cc659dc2b7919054aba4f6
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-05-09 08:02:02 -0700

    style on map and calendar for desktop and mobile

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -2,14 +2,15 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Nav from "../components/Nav";
 import MapTest from "../assets/images/mapTest.jpg";
+import "./style.css"
 
 function Map() {
   return (
     <div>
       <Nav />
-      <Jumbotron className="maincontain" fluid>
+      <Jumbotron className="mobilemaptron" fluid>
         <div>
-          <h5>Event Locator:</h5>
+          <h5 className="mobilemaptron" >Event Locator</h5>
           <img
             src={MapTest}
             style={{ maxWidth: 375 }}
